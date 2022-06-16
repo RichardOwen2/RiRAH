ab043df2a2795a93a9a074abae6857c1d25f12a2
RichardOwen2 <104406943+RichardOwen2@users.noreply.github.com>
2022-06-16 20:15:29 +0800

    Add files via upload

---
+++ Page/bgDiterima.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Page;
+
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+
+public class bgDiterima extends JPanel{
+    Image gambar;
+    public bgDiterima (){
+        gambar = new ImageIcon(getClass().getResource("/Iconn/pesananDiterima.png")).getImage();
+    }
+    @Override
+    protected void paintComponent (Graphics g){
+        super.paintComponent(g);
+        Graphics2D gd = (Graphics2D)g.create();
+        gd.drawImage(gambar,0,0,getWidth(),getHeight(),null);
+        gd.dispose();
+    }
+}
---
+++ Page/bgRegis.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Page;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+
+public class bgRegis extends JPanel{
+    Image gambar;
+    public bgRegis (){
+        gambar = new ImageIcon(getClass().getResource("/Iconn/REGISTER.png")).getImage();
+    }
+    @Override
+    protected void paintComponent (Graphics g){
+        super.paintComponent(g);
+        Graphics2D gd = (Graphics2D)g.create();
+        gd.drawImage(gambar,0,0,getWidth(),getHeight(),null);
+        gd.dispose();
+    }
+}
